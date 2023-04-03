9d2a0f372e276687e040577a993423e104d4cd16
Arman <adantizzz@gmail.com>
2023-04-03 20:26:37 +0600

    asd

--- script.js
+++
@@ -1,7 +0,0 @@
-const entranceBtn = document.querySelector(".entrance");
-const registrBtn = document.querySelector(".registration");
-
-entranceBtn.addEventListener("click", (e) => {
-   
-});
-registrBtn.addEventListener("click", (e) => {});
