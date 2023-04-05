9e35243ef92700c733ebfd355a0b82ce177b29c6
WildMaks456 <polynkovmaksim098@gmail.com>
2023-04-05 19:26:25 +0600

    chats-index

--- Maxim/script.js
+++ Maxim/script.js
@@ -1,1 +1,41 @@
-//
+// fetch("", {
+// 	method: "POST",
+// 	mode: "cors",
+// 	headers: {
+// 		"Content-type": "application/json"
+// 	},
+// 	body: d,
+// })
+
+
+
+// const socket = new WebSocket('ws://172.28.0.202:8000/ws');
+
+// const int = document.querySelector("#inp");
+// const form = document.querySelector("form");
+
+// socket.addEventListener("open", (event) => {
+// 	console.log("Websocket connection established");
+
+// 	form.addEventListener("submit", (e) => {
+// 		e.preventDefault();
+// 		const message = {
+// 			chatId: 0,
+// 			text: inp.value,
+// 			date: new Date()
+// 		}
+// 		socket.send(JSON.stringify(message.text));
+// 	})
+// })
+
+// socket.addEventListener("message", (event) => {
+// 	console.log(event.data);
+// })
+
+// socket.addEventListener("close", (event) => {
+// 	console.log("Websocket connection closed");
+// })
+
+// socket.addEventListener("error", (event) => {
+// 	console.error("Websocket error", event);
+// })
